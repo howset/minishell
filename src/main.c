f4d83562b9678d6ba57721c26d3df1e3f8aa00cc
Howard Setyamukti <hsetyamu@c4b3c6.42berlin.de>
2024-11-18 17:39:28 +0100

    Now can exit

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: hsetyamu <hsetyamu@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 15:09:08 by hsetyamu          #+#    #+#             */
-/*   Updated: 2024/11/18 17:17:34 by hsetyamu         ###   ########.fr       */
+/*   Updated: 2024/11/18 17:38:47 by hsetyamu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,9 @@ int main()
 	{	
 		input = readline("wtf-shell> "); //display prompt
 		add_history(input); //add input to readline history
-		ft_printf("input is:%s\n", input); //just a feedback (useful for echo??)
+		if (ft_strncmp(input,"exit", 4) == 0)
+			exit(0);
+		printf("input is:%s\n", input); //just a feedback (useful for echo??)
 		free(input);
 	}
 	return 0;
