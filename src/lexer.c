a20f2f89a74d87b0784cdbb16f2fba3abfbfd3b2
Howard Setyamukti <hsetyamu@c4b3c4.42berlin.de>
2024-11-22 14:58:17 +0100

    remove old lexer func

--- src/lexer.c
+++ src/lexer.c
@@ -6,7 +6,7 @@
 /*   By: hsetyamu <hsetyamu@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 15:30:54 by hsetyamu          #+#    #+#             */
-/*   Updated: 2024/11/22 14:34:02 by hsetyamu         ###   ########.fr       */
+/*   Updated: 2024/11/22 14:56:36 by hsetyamu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,54 +18,6 @@ int	ft_isspace(char c)
 		return (1);
 	return (0);
 }
-
-/* void	lexer(const char *input, Token tokens[], int *token_count) 
-{
-	const char	*current = input;
-	int			count = 0;
-
-	while (*current != '\0') 
-	{
-		while (ft_isspace(*current)) // Skip whitespace
-			current++;
-		if (ft_isalpha(*current)) // Commands
-		{
-			int length = 0;
-			while (ft_isalnum(*current))
-			{
-				tokens[count].value[length++] = *current++;
-			}
-			tokens[count].value[length] = '\0';
-			tokens[count].type = TOKEN_COMMANDS;
-			count++;
-		} 
-		else if (ft_isdigit(*current)) // Arguments
-		{
-			int length = 0;
-			while (ft_isdigit(*current)) 
-				tokens[count].value[length++] = *current++;
-			tokens[count].value[length] = '\0';
-			tokens[count].type = TOKEN_ARGUMENTS;
-			count++;
-		} 
-		else if (*current == '|' || *current == '<' || *current == '>') // Redirs, problem: what about << & >>
-		{
-			tokens[count].value[0] = *current++;
-			tokens[count].value[1] = '\0';
-			tokens[count].type = TOKEN_REDIRECTIONS;
-			count++;
-		}
-		else // Unknown character, skip
-			current++;
-	}
-
-	// End token
-	tokens[count].type = TOKEN_END;
-	strcpy(tokens[count].value, "END");
-	count++;
-
-//	*token_count = count;
-} */
 
 // Lexer function
 t_token *lexer(const char *input, int *token_count) 
@@ -90,12 +42,6 @@ t_token *lexer(const char *input, int *token_count)
 			length = i - start;
 			token.type = TKN_WORD;
 			token.value = malloc_perex(length + 1, "Failed on words");
-/* 			token.value = malloc(length + 1);
-			if (!token.value) 
-			{
-				perror("Malloc failed for token.value (words)");
-				exit(EXIT_FAILURE);
-			} */
 			ft_strlcpy(token.value, &input[start], length + 1);
 		}
 		else
@@ -110,12 +56,6 @@ t_token *lexer(const char *input, int *token_count)
 			length = i - start;
 			token.type = TKN_METACHAR;
 			token.value = malloc_perex(length + 1, "Failed on chars");
-			/* token.value = malloc(length + 1);
-			if (!token.value)
-			{
-				perror("Malloc failedfor token.value (metachars)");
-				exit(EXIT_FAILURE);
-			} */
 			ft_strlcpy(token.value, &input[start], length + 1);
 		}
 		if (*token_count < ARG_LEN)
