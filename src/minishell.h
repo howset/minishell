1683f0a2d96ce02114dd06d7936b876118ee11ea
Reem Eldahlish <reemy@Reems-MacBook-Air.local>
2024-11-22 16:34:47 +0100

    New lexer function

--- src/minishell.h
+++ src/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hsetyamu <hsetyamu@student.42berlin.de>    +#+  +:+       +#+        */
+/*   By: reldahli <reldahli@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 17:08:00 by hsetyamu          #+#    #+#             */
-/*   Updated: 2024/11/21 18:46:55 by hsetyamu         ###   ########.fr       */
+/*   Updated: 2024/11/22 16:02:33 by reldahli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,10 @@
 # include "./lib/src/get_next_line/get_next_line.h"
 # include "./lib/src/libft/libft.h"
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <readline/readline.h>
-#include <readline/history.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
 
 /* typedef enum {
 	TOKEN_COMMANDS, //0 Token command
@@ -36,22 +36,28 @@
 	TKN_END,
 }	t_tkntype; */
 
-#define ARG_LEN 100 //arrange by argc??
+# define ARG_LEN 100 //arrange by argc??
 
 typedef enum e_tkntype
 {
-	TKN_WORD,
-	TKN_METACHAR,
+	TKN_WORD, //everything (incl keywords, commands, others)
+	TKN_PIPE, //|
+	TKN_REDIRECT_IN, //<
+	TKN_REDIRECT_OUT, //>
+	TKN_REDIRECT_APPEND, //>>
+	TKN_HEREDOC //<<
 }	t_tkntype;
 
-typedef struct s_token{
-	t_tkntype	type;
-	char		*value;
+typedef struct s_token
+{
+	t_tkntype		type;
+	char			*value;
+	int				position; //position in the input string
+	struct s_token	*next; //pointer to the next token (for linked list implementation)
 }	t_token;
 
 int	ft_isspace(char c);
 //void	lexer(const char *input, Token tokens[], int *token_count) ;
-t_token *lexer(const char *input, int *token_count);
-void free_tokens(t_token *tokens, size_t token_count);
-
+void		free_tokens(t_token *tokens, size_t token_count);
+t_token		*lexer(const char *input, int *token_count);
 #endif
