6e85d6c6eb0973c99aad8e1bf82f5279c9a9659b
Reem Essameldin <reem.essam.eldin@gmail.com>
2024-12-25 13:04:55 +0100

    added builtins tests

---
+++ tests/builtins/builtin_pwd_test.c
@@ -0,0 +1,70 @@
+
+#include "../../src/minishell.h"
+#include "../../src/parser/parser.h"
+#include <check.h>
+
+START_TEST(test_pwd_success)
+{
+	char	*expected;
+	int		ret;
+
+	expected = getcwd(NULL, 0);
+	ret = rh_pwd();
+	ck_assert_int_eq(ret, EXIT_SUCCESS);
+	free(expected);
+}
+END_TEST
+
+START_TEST(test_cd_success)
+{
+	char	*old_dir;
+	char	*home;
+	int		ret;
+
+	old_dir = getcwd(NULL, 0);
+	home = getenv("HOME");
+	ret = rh_cd(home);
+	ck_assert_int_eq(ret, EXIT_SUCCESS);
+	ret = rh_cd(old_dir);
+	ck_assert_int_eq(ret, EXIT_SUCCESS);
+	free(old_dir);
+}
+END_TEST
+
+START_TEST(test_cd_null_path)
+{
+	char	*old_dir;
+	int		ret;
+
+	old_dir = getcwd(NULL, 0);
+	ret = rh_cd(NULL);
+	ck_assert_int_eq(ret, EXIT_SUCCESS);
+	ret = rh_cd(old_dir);
+	ck_assert_int_eq(ret, EXIT_SUCCESS);
+	free(old_dir);
+}
+END_TEST
+
+START_TEST(test_cd_invalid_path)
+{
+	int	ret;
+
+	ret = rh_cd("/nonexistent/path");
+	ck_assert_int_eq(ret, EXIT_FAILURE);
+}
+END_TEST
+
+Suite	*pwd_suite(void)
+{
+	Suite	*s;
+	TCase	*tc_core;
+
+	s = suite_create("pwd");
+	tc_core = tcase_create("Core");
+	tcase_add_test(tc_core, test_pwd_success);
+	tcase_add_test(tc_core, test_cd_success);
+	tcase_add_test(tc_core, test_cd_null_path);
+	tcase_add_test(tc_core, test_cd_invalid_path);
+	suite_add_tcase(s, tc_core);
+	return (s);
+}
--- tests/main_test.c
+++ tests/main_test.c
@@ -2,39 +2,42 @@
 #include <stdlib.h>
 
 // Existing suite declarations
-Suite *lexer_grouping_suite(void);
-Suite *lexer_operator_suite(void);
-Suite *lexer_quote_suite(void);
-Suite *lexer_redirection_suite(void);
-Suite *lexer_word_suite(void);
+Suite	*lexer_grouping_suite(void);
+Suite	*lexer_operator_suite(void);
+Suite	*lexer_quote_suite(void);
+Suite	*lexer_redirection_suite(void);
+Suite	*lexer_word_suite(void);
 
 // New parser suite declarations
-Suite *parser_simple_command_suite(void);
-Suite *parser_pipe_suite(void);
-Suite *parser_redirection_suite(void);
-Suite *parser_logical_suite(void);
+Suite	*parser_simple_command_suite(void);
+Suite	*parser_pipe_suite(void);
+Suite	*parser_redirection_suite(void);
+Suite	*parser_logical_suite(void);
 
-int main(void)
+// Builtins Suite
+Suite	*pwd_suite(void);
+
+int	main(void)
 {
-    int number_failed;
-    SRunner *sr = srunner_create(NULL);
+	int		number_failed;
+	SRunner	*sr;
 
-    // Add lexer suites
-    srunner_add_suite(sr, lexer_grouping_suite());
-    srunner_add_suite(sr, lexer_operator_suite());
-    srunner_add_suite(sr, lexer_quote_suite());
-    srunner_add_suite(sr, lexer_redirection_suite());
-    srunner_add_suite(sr, lexer_word_suite());
-
-    // Add parser suites
-    srunner_add_suite(sr, parser_simple_command_suite());
-    srunner_add_suite(sr, parser_pipe_suite());
-    srunner_add_suite(sr, parser_redirection_suite());
-    srunner_add_suite(sr, parser_logical_suite());
-
-    srunner_run_all(sr, CK_NORMAL);
-    number_failed = srunner_ntests_failed(sr);
-    srunner_free(sr);
-
-    return (number_failed == 0) ? 0 : 1;
+	sr = srunner_create(NULL);
+	// Add lexer suites
+	srunner_add_suite(sr, lexer_grouping_suite());
+	srunner_add_suite(sr, lexer_operator_suite());
+	srunner_add_suite(sr, lexer_quote_suite());
+	srunner_add_suite(sr, lexer_redirection_suite());
+	srunner_add_suite(sr, lexer_word_suite());
+	// Add parser suites
+	srunner_add_suite(sr, parser_simple_command_suite());
+	srunner_add_suite(sr, parser_pipe_suite());
+	srunner_add_suite(sr, parser_redirection_suite());
+	srunner_add_suite(sr, parser_logical_suite());
+	// Add builtins suites
+	srunner_add_suite(sr, pwd_suite());
+	srunner_run_all(sr, CK_NORMAL);
+	number_failed = srunner_ntests_failed(sr);
+	srunner_free(sr);
+	return ((number_failed == 0) ? 0 : 1);
 }
