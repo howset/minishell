69fb9916fc7ccbfa5bc5af235bafb15e96453957
Reem Essameldin <reem.essam.eldin@gmail.com>
2024-12-28 22:32:33 +0100

    improve tttttttttests

--- tests/parser/parser_pipe_test.c
+++ tests/parser/parser_pipe_test.c
@@ -1,6 +1,5 @@
 #include "../tests.h"
 
-
 START_TEST(test_parse_pipe)
 {
 	t_token	*tokens;
@@ -61,6 +60,42 @@ START_TEST(test_parse_multiple_pipe)
 }
 END_TEST
 
+START_TEST(test_multiple_parsre_with_redirection)
+{
+	t_token	*tokens;
+	t_ast	*ast;
+
+	tokens = lexer("ls -l | grep test | wc -l > output.txt");
+	ast = parse(tokens);
+	ck_assert_ptr_nonnull(ast);
+	ck_assert_int_eq(ast->type, NODE_PIPE);
+	// First Pipe
+	ck_assert_ptr_nonnull(ast->left);
+	ck_assert_int_eq(ast->left->type, NODE_PIPE);
+	ck_assert_int_eq(ast->left->args_count, 0);
+	// First command (ls -l)
+	ck_assert_ptr_nonnull(ast->left);
+	ck_assert_int_eq(ast->left->left->type, NODE_COMMAND);
+	ck_assert_int_eq(ast->left->left->args_count, 2);
+	ck_assert_str_eq(ast->left->left->args[0], "ls");
+	ck_assert_str_eq(ast->left->left->args[1], "-l");
+	// Second pipe and grep command
+	ck_assert_ptr_nonnull(ast->left->right);
+	ck_assert_int_eq(ast->left->right->type, NODE_COMMAND);
+	ck_assert_int_eq(ast->left->right->args_count, 2);
+	ck_assert_str_eq(ast->left->right->args[0], "grep");
+	ck_assert_str_eq(ast->left->right->args[1], "test");
+	// Last command (wc -l)
+	ck_assert_ptr_nonnull(ast->right);
+	ck_assert_int_eq(ast->right->type, NODE_REDIRECTION);
+	ck_assert_int_eq(ast->right->redirection_type, TKN_RDIR_OUT);
+	ck_assert_str_eq(ast->right->filename, "output.txt");
+	ck_assert_str_eq(ast->right->left->args[0], "wc");
+	free_ast(ast);
+	free_tkn(tokens);
+}
+END_TEST
+
 Suite	*parser_pipe_suite(void)
 {
 	Suite	*s;
@@ -70,6 +105,7 @@ Suite	*parser_pipe_suite(void)
 	tc_core = tcase_create("Core");
 	tcase_add_test(tc_core, test_parse_pipe);
 	tcase_add_test(tc_core, test_parse_multiple_pipe);
+	tcase_add_test(tc_core, test_multiple_parsre_with_redirection);
 	suite_add_tcase(s, tc_core);
 	return (s);
 }
