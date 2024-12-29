41273cd07d072eed633e3977d65b2a97dc4bef22
Reem Essameldin <reem.essam.eldin@gmail.com>
2024-12-29 11:46:04 +0100

    moved builtin command check to exec_chprocess

--- src/exec/exec.c
+++ src/exec/exec.c
@@ -1,59 +1,5 @@
 #include "./exec.h"
 
-/**This function just serves as a check if the given simple command is included
- * as one of the built-in functions or not.
- * 		Takes the command (args[0] in the t_simmcom struct) as an argument.
- * 		Returns a true/false (1/0 -bool)
- */
-int	is_builtin(char *cmd)
-{
-	if (ft_strncmp(cmd, "echo", 4) == 0)
-		return (1);
-	else if (ft_strncmp(cmd, "cd", 2) == 0)
-		return (1);
-	else if (ft_strncmp(cmd, "pwd", 3) == 0)
-		return (1);
-	else if (ft_strncmp(cmd, "export", 6) == 0)
-		return (1);
-	else if (ft_strncmp(cmd, "unset", 5) == 0)
-		return (1);
-	else if (ft_strncmp(cmd, "env", 3) == 0)
-		return (1);
-	else if (ft_strncmp(cmd, "exit", 4) == 0)
-		return (1);
-	else
-		return (0);
-}
-
-/**This function calls the corresponding built-in functions. Cuurently only
- * echo is operational (still WIP). Haven't begun on the others yet.
- * 		Takes the field args from a t_simmcom (row on t_commtab). And now also
- * 			env_list and envp for functions that required them (e.g. export).
- * 		Returns an exit status.
- */
-int	exec_builtin(char *args[], t_env **env_list, char *envp[])
-{
-	int	exit_stat;
-
-	if (ft_strncmp(args[0], "echo", 4) == 0)
-		exit_stat = rh_echo(args);
-	else if (ft_strncmp(args[0], "exit", 4) == 0)
-		exit_stat = rh_exit(args);
-	else if (ft_strncmp(args[0], "env", 3) == 0)
-		exit_stat = rh_env(args, envp, env_list);
-	else if (ft_strncmp(args[0], "export", 6) == 0)
-		exit_stat = rh_export(args, env_list);
-	else if (ft_strncmp(args[0], "unset", 5) == 0)
-		exit_stat = rh_unset(args, env_list);
-	else if (ft_strncmp(args[0], "cd", 2) == 0)
-		exit_stat = rh_cd(args[1]);
-	else if (ft_strncmp(args[0], "pwd", 3) == 0)
-		exit_stat = rh_pwd();
-	else
-		exit_stat = 1;
-	return (exit_stat);
-}
-
 /**This function executes the simple commands from the command table
  * sequentially, row-by-row. A check for built-in functions is performed
  * and the corresponding function will be called appropriately (exec_prog for
@@ -71,9 +17,7 @@ int	exec_commtab(t_cmdtable *table, t_env **env_list, char *envp[])
 	while (i < table->cmd_count)
 	{
 		cmd = &table->commands[i];
-		if (is_builtin(cmd->args[0]))
-			exit_stat = exec_builtin(cmd->args, env_list, envp);
-		else if (cmd->type == CMD_SIMPLE)
+		if (cmd->type == CMD_SIMPLE)
 			exit_stat = exec_simple_command(cmd, *env_list, envp);
 		else if (cmd->type == CMD_PIPE)
 		{
