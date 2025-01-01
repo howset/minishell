bf2e3399f3a91b3ba4b7d35740e4010d98a1c626
Reem Essameldin <reem.essam.eldin@gmail.com>
2025-01-01 18:44:57 +0100

    Rewrite parse_term to handle complex redirections

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,11 +6,23 @@
 /*   By: reldahli <reldahli@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/20 17:33:30 by reldahli          #+#    #+#             */
-/*   Updated: 2024/12/30 14:26:18 by reldahli         ###   ########.fr       */
+/*   Updated: 2025/01/01 18:41:17 by reldahli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./parser.h"
+
+static int	is_argument_token(t_tkntype type)
+{
+	return (type == TKN_WORD || type == TKN_VAR || type == TKN_QUO_SIN
+		|| type == TKN_QUO_DOU || type == TKN_BG);
+}
+
+static int	is_redirection_token(t_tkntype type)
+{
+	return (type == TKN_RDIR_IN || type == TKN_RDIR_OUT || type == TKN_APPEND
+		|| type == TKN_HEREDOC);
+}
 
 t_ast	*parse_command(t_token **current, t_alldata *all_data)
 {
@@ -70,29 +82,79 @@ t_ast	*parse_term(t_token **current, t_alldata *all_data)
 {
 	t_ast	*node;
 	t_ast	*redir_node;
+	t_ast	*command_node;
 
+	// First parse either a simple command, variable, etc., or '(' expression
 	node = parse_factor(current, all_data);
-	while (*current && ((*current)->type == TKN_RDIR_IN
-			|| (*current)->type == TKN_RDIR_OUT
-			|| (*current)->type == TKN_APPEND
-			|| (*current)->type == TKN_HEREDOC))
+	if (!node)
+		return (NULL);
+	/*
+		* Keep looping if the next token is either:
+		*  -- a redirection operator (<, >, >>, <<),
+		*  -- or a token that can be appended as an argument (TKN_WORD, TKN_QUO,
+			TKN_VAR...).
+		* This allows us to handle sequences like:
+		*   echo hi < file more_args >> another_file "another arg"
+		*/
+	while (*current && (is_redirection_token((*current)->type)
+			|| is_argument_token((*current)->type)))
 	{
-		redir_node = create_ast_node(NODE_REDIRECTION);
-		redir_node->redirection_type = (*current)->type;
-		(*current) = (*current)->next;
-		if ((*current)->type != TKN_WORD)
+		// If it's a redirection operator, parse it
+		if (is_redirection_token((*current)->type))
 		{
-			if (*current)
-				syntax_error_at((*current)->position,
-					"Expected a filename after redirection");
-			else
-				syntax_error_at(-1, "Expected a filename after redirection");
-			return (NULL);
+			redir_node = create_ast_node(NODE_REDIRECTION);
+			if (!redir_node)
+				return (NULL);
+			redir_node->redirection_type = (*current)->type;
+			(*current) = (*current)->next; // move past '<' or '>' etc.
+			// Next token must be a valid "filename" token
+			if (!(*current) || !is_argument_token((*current)->type))
+			{
+				if (*current)
+					syntax_error_at((*current)->position,
+						"Expected a filename after redirection");
+				else
+					syntax_error_at(-1,
+						"Expected a filename after redirection");
+				return (NULL);
+			}
+			redir_node->filename = ft_strdup(sanitize_text((*current)->value,
+						all_data));
+			(*current) = (*current)->next; // consume the filename token
+			// attach previous node (command or pipeline or subshell) to the redirection's left
+			redir_node->left = node;
+			node = redir_node;
 		}
-		redir_node->filename = ft_strdup((*current)->value);
-		(*current) = (*current)->next;
-		redir_node->left = node;
-		node = redir_node;
+		// Otherwise, if it's an argument token, append it to the command node
+		else
+		{
+			/*
+				* We need to find the underlying command node to attach extra arguments.
+				* E.g. if 'node' is a chain of redirections,
+					go down .left until we find
+				* the actual NODE_COMMAND (echo, cat, grep, etc.).
+				*/
+			command_node = node;
+			while (command_node && command_node->type == NODE_REDIRECTION)
+				command_node = command_node->left;
+			// If there's no command node at all, we can optionally create one
+			// or throw an error. For a typical Bash-like shell, you might
+			// create a new command node, but let's keep it simple:
+			if (!command_node || command_node->type != NODE_COMMAND)
+			{
+				syntax_error_at((*current)->position,
+					"Unexpected argument token");
+				return (NULL);
+			}
+			// Append the new argument
+			command_node->args_count++;
+			command_node->args = realloc(command_node->args, sizeof(char *)
+					* (command_node->args_count + 1));
+			command_node->args[command_node->args_count
+				- 1] = ft_strdup(sanitize_text((*current)->value, all_data));
+			command_node->args[command_node->args_count] = NULL;
+			(*current) = (*current)->next; // consume the argument token
+		}
 	}
 	return (node);
 }
