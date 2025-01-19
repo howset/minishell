ebc2ce16126abc9a30615bd639263b3068beb95c
howset <howsetya@googlemail.com>
2025-01-19 02:13:40 +0100

    docs in structs.h

--- src/structs.h
+++ src/structs.h
@@ -6,7 +6,7 @@
 /*   By: hsetya <hsetya@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/30 03:36:00 by reldahli          #+#    #+#             */
-/*   Updated: 2025/01/19 01:04:19 by hsetya           ###   ########.fr       */
+/*   Updated: 2025/01/19 02:11:45 by hsetya           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,14 +36,6 @@ typedef enum e_tkntype
 	TKN_INVALID,
 }					t_tkntype;
 
-typedef struct s_token
-{
-	t_tkntype		type;
-	char			*value;
-	int				position;
-	struct s_token	*next;
-}					t_token;
-
 typedef enum e_nodetype
 {
 	NODE_COMMAND,
@@ -56,16 +48,6 @@ typedef enum e_nodetype
 	NODE_INVALID,
 }					t_nodetype;
 
-typedef struct s_ast
-{
-	t_nodetype		type;
-	struct s_ast	*left;
-	struct s_ast	*right;
-	char			**args;
-	int				args_count;
-	t_tkntype		redirection_type;
-	char			*filename;
-}					t_ast;
 /*
  * Command type enumeration to identify different types of commands
  * in the command table
@@ -77,15 +59,89 @@ typedef enum e_cmd_type
 	CMD_SUBSHELL,
 }					t_cmd_type;
 
+/**
+ * @struct s_token
+ * @brief Represents a token in the minishell.
+ *
+ * This structure is used to represent a token in the minishell. A token
+ * consists of a type, a value, a position, and a pointer to the next token
+ * in the list.
+ *
+ * @var t_token::type
+ * 	The type of the token, represented by the t_tkntype enumeration.
+ * @var t_token::value
+ * 	The value of the token, stored as a string.
+ * @var t_token::position
+ * 	The position of the token in the input string.
+ * @var t_token::next
+ * 	A pointer to the next token in the list.
+ */
+typedef struct s_token
+{
+	t_tkntype		type;
+	char			*value;
+	int				position;
+	struct s_token	*next;
+}					t_token;
+
+/**
+ * @struct s_ast
+ * @brief Represents an abstract syntax tree (AST) node.
+ *
+ * This structure is used to represent nodes in an abstract syntax tree,
+ * which is a hierarchical tree representation of the structure of source code.
+ *
+ * @var t_ast::type
+ * 	The type of the node, represented by the t_nodetype enum.
+ * @var t_ast::left
+ * 	Pointer to the left child node in the AST.
+ * @var t_ast::right
+ * 	Pointer to the right child node in the AST.
+ * @var t_ast::args
+ * 	Array of arguments associated with the node.
+ * @var t_ast::args_count
+ * 	The number of arguments in the args array.
+ * @var t_ast::redirection_type
+ * 	The type of redirection associated with the node, represented by the t_tkntype enum.
+ * @var t_ast::filename
+ * 	The filename associated with the redirection, if any.
+ */
+typedef struct s_ast
+{
+	t_nodetype		type;
+	struct s_ast	*left;
+	struct s_ast	*right;
+	char			**args;
+	int				args_count;
+	t_tkntype		redirection_type;
+	char			*filename;
+}					t_ast;
+
 /*
  * Redirection structure to store input/output redirections
+ */
+/**
+ * @struct s_redirection
+ * @brief Represents a redirection in a shell command.
+ *
+ * This structure is used to store information about a redirection
+ * in a shell command, including the type of redirection, the file
+ * associated with the redirection, and a pointer to the next
+ * redirection in the list.
+ *
+ * @var s_redirection::type
+ * 	The type of the redirection (e.g., input, output, append).
+ * @var s_redirection::file
+ * 	The file associated with the redirection.
+ * @var s_redirection::next
+ * 	Pointer to the next redirection in the list.
  */
 typedef struct s_redirection
 {
 	t_tkntype				type;
 	char					*file;
 	struct s_redirection	*next;
-}					t_redirection;
+}							t_redirection;
 
 /*
  * Command structure to store a single command with its arguments
@@ -99,7 +155,7 @@ typedef struct s_command
 	int					pipe_read;
 	int					pipe_write;
 	struct s_command	*next;
-}							t_command;
+}						t_command;
 
 /*
  * Command table structure to store all commands to be executed
@@ -111,6 +167,21 @@ typedef struct s_cmdtable
 	int				pipe_count;
 }					t_cmdtable;
 
+/**
+ * @struct s_env
+ * @brief A structure to represent an environment variable.
+ *
+ * This structure is used to store an environment variable as a key-value pair.
+ * It also includes a pointer to the next environment variable in the list,
+ * allowing the creation of a linked list of environment variables.
+ *
+ * @var s_env::key
+ * 	The key of the environment variable (e.g., "PATH").
+ * @var s_env::val
+ * 	The value of the environment variable (e.g., "/usr/bin").
+ * @var s_env::next
+ * 	A pointer to the next environment variable in the linked list.
+ */
 typedef struct s_env
 {
 	char			*key;
@@ -118,6 +189,21 @@ typedef struct s_env
 	struct s_env	*next;
 }					t_env;
 
+/**
+ * @struct s_pipeline_data
+ * @brief Structure to hold data related to a pipeline of commands.
+ *
+ * @var s_pipeline_data::last_cmd
+ * 	Pointer to the last command in the pipeline.
+ * @var s_pipeline_data::env_list
+ * 	Pointer to an array of environment variables.
+ * @var s_pipeline_data::envp
+ * 	Pointer to an array of environment strings.
+ * @var s_pipeline_data::prev_pipe
+ * 	Array to hold the file descriptors for the previous pipe.
+ * @var s_pipeline_data::status
+ * 	Integer to hold the status of the pipeline execution.
+ */
 typedef struct s_pipeline_data
 {
 	t_command	*last_cmd;
@@ -125,8 +211,31 @@ typedef struct s_pipeline_data
 	char		**envp;
 	int			prev_pipe[2];
 	int			status;
-}	t_pipeline_data;
+}				t_pipeline_data;
 
+/**
+ * @struct s_alldata
+ * @brief A structure to hold all data related to the minishell program.
+ *
+ * This structure contains various elements that are used throughout the
+ * minishell program, including user input, tokens, abstract syntax tree,
+ * command table, exit status, and environment variables.
+ *
+ * @var s_alldata::input
+ * 	A pointer to a string that holds the user input.
+ * @var s_alldata::tokens
+ * 	A pointer to a list of tokens generated from the user input.
+ * @var s_alldata::tree
+ * 	A pointer to the abstract syntax tree (AST) generated from the tokens.
+ * @var s_alldata::table
+ * 	A pointer to the command table generated from the AST.
+ * @var s_alldata::exit_stat
+ * 	An integer representing the exit status of the last executed command.
+ * @var s_alldata::env_head
+ * 	A pointer to the head of the environment variables linked list.
+ * @var s_alldata::env_list
+ * 	A pointer to an array of pointers to environment variables.
+ */
 typedef struct s_alldata
 {
 	char			*input;
